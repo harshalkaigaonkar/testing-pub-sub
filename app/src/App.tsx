7c84b47e82c3f20b2801798452ab0e2da477f340
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2024-01-07 01:45:22 +0530

    pub sub testing success

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const handlePublish = () => {
+    axios.post("http://localhost:3001/publish", {});
+  }
+  const handleSubscribe = () => {
+    axios.post("http://localhost:3001/subscribe", {});
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleSubscribe}>Subscribe</button>
+        <button onClick={handlePublish}>Publish</button>
       </header>
     </div>
   );
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { createClient } from 'redis';
 
 dotenv.config();
 
+const redis_url = "redis://default:tnvrtGiMb1tvm8Pj7ML7SV2W3kEu5Ue3@redis-14001.c325.us-east-1-4.ec2.cloud.redislabs.com:14001"
+
 const app : Express = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
+const client = createClient({
+  url: redis_url
+})
+client.connect();
+
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+app.post('/subscribe', async (req: Request, res: Response) => {
+  await client.subscribe("channel", (message, channel) => {
+    console.log('subscribed to channel', message, channel)
+  })
+  res.send("done")
+})
+app.post('/publish', async (req: Request, res: Response) => {
+const publisher = client.duplicate();
+await publisher.connect();
+  await publisher.publish("channel", "{Hello}");
+  res.send("done")
+})
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
